984dde193393873d27b3f8e977004145826eebb5
nguyeti <>
2017-06-11 12:59:56 -0400

    pl/sql - functions

---
+++ plsql/functions.sql
@@ -0,0 +1,14 @@
+-- Function for calculating the area of a circle
+CREATE OR REPLACE FUNCTION circle_area (radius NUMBER)
+RETURN NUMBER IS
+
+pi CONSTANT NUMBER(7,3) := 3.141;
+area NUMBER(7,3);
+
+BEGIN
+    area := pi * POWER(radius,2);
+    RETURN area;
+END;
+/
+
+select circle_area(20) from dual;
